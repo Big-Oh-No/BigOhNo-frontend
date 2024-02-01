548fe63361e0a4fd156e0dbfd856e181c7e7bc5c
moon <113469325+Moon0256@users.noreply.github.com>
2024-01-31 20:14:42 -0800

    init 🎉

---
+++ src/components/Demo.jsx
@@ -0,0 +1,9 @@
+function Demo() {
+    return (
+        <div className="w-full h-full flex justify-center items-center bg-white rounded-xl text-black">
+            Meow
+        </div>
+    );
+}
+
+export default Demo;
