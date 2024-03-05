940c6bc83dc284ff3221c07b7e5e649e1f720c02
Anand <clef@Anands-MacBook-Pro.local>
2024-03-05 14:10:02 -0800

    UI updates

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -4,27 +4,34 @@ export default function SignUpForm() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col justify-center w-full h-full pt-32 pl-8">
-      <div className=" flex flex-col text-4xl font-bold ">Sign Up</div>
-      <div className="flex flex-row justify-between text-xl font-semibold pr-52">
-        <div className="">First Name</div>
-        <div className=" ">Last Name</div>
+    <div className="flex flex-col w-full h-full px-20 pt-24">
+      <div className="text-5xl font-bold font-inter">
+        Welcome to <div className="text-dark-theme inline">Korse!</div>
       </div>
-      <div className="flex flex-row justify-between">
-        <div className="w-[40%] focus:outline-none bg-light-theme border-[0.075rem] border-black h-14 text-lg rounded-xl"></div>
-        <div className="w-[40%] focus:outline-none bg-light-theme border-[0.075rem] border-black h-14 text-lg rounded-xl"></div>
+      <div className=" flex flex-col text-4xl font-bold mt-20">Sign Up</div>
+      <div className="flex flex-row justify-between mt-8">
+        <div className="w-[45%] space-y-1">
+          <div className="text-xl font-semibold">First Name</div>
+          <input className="px-5 w-full focus:outline-none bg-light-theme border-[0.075rem] border-black h-14 text-lg rounded-xl"></input>
+        </div>
+        <div className="w-[45%] space-y-1">
+          <div className="text-xl font-semibold">Last Name</div>
+          <input className="px-5 w-full focus:outline-none bg-light-theme border-[0.075rem] border-black h-14 text-lg rounded-xl"></input>
+        </div>
       </div>
-      <div className="text-xl font-semibold">
-        <div>Email</div>
-        <div className="w-full focus:outline bg-light-theme border-[0.075rem] border-black h-14 text-lg rounded-xl "></div>
+      <div className="mt-5 flex flex-col space-y-1">
+        <div className="text-xl font-semibold">Email</div>
+        <input className="px-5 w-full focus:outline-none bg-light-theme border-[0.075rem] border-black h-14 text-lg rounded-xl "></input>
       </div>
-      <div className="flex flex-row justify-between text-xl font-semibold pr-32">
-        <div>Create Password</div>
-        <div>Confirm Password</div>
-      </div>
-      <div className="flex flex-row justify-between">
-        <div className="w-[40%] focus:outline-none bg-light-theme border-[0.075rem] border-black h-14 text-lg rounded-xl"></div>
-        <div className="w-[40%] focus:outline-none bg-light-theme border-[0.075rem] border-black h-14 text-lg rounded-xl"></div>
+      <div className="flex flex-row justify-between mt-5">
+        <div className="flex flex-col w-[45%] space-y-1">
+          <div className="text-xl font-semibold">Create Password</div>
+          <input type="password" className="px-5 w-full focus:outline-none bg-light-theme border-[0.075rem] border-black h-14 text-lg rounded-xl"></input>
+        </div>
+        <div className="flex flex-col w-[45%] space-y-1">
+          <div className="text-xl font-semibold">Confirm Password</div>
+          <input type="password" className="px-5 w-full focus:outline-none bg-light-theme border-[0.075rem] border-black h-14 text-lg rounded-xl"></input>
+        </div>
       </div>
     </div>
   );
--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -1,15 +1,19 @@
-import SignUpForm from "../components/SignUpForm"
+import SignUpForm from "../components/SignUpForm";
 export default function SignUp() {
   return (
-  <div className="w-screen h-screen bg-white">
-    <div className="text-5xl font-bold absolute top-10 left-10 font-inter">
-        Welcome to <div className="text-dark-theme inline">Korse!</div>
+    <div className="w-screen h-screen bg-white">
+      <img
+        src={require("../assets/sign_up.png")}
+        alt="SignUp Page"
+        className="absolute bottom-40 right-0"
+      />
+      <div className="h-full w-[57%]">
+        <SignUpForm />
+      </div>
+      <div className="w-full h-10 absolute bottom-0 flex flex-row justify-center">
+        Already have an account?{" "}
+        <div className="text-dark-theme inline font-semibold ml-1 hover:underline hover:cursor-pointer">Sign In</div>
+      </div>
     </div>
-    <img src={require("../assets/sign_up.png")} alt="SignUp Page" className="absolute bottom-40 right-0"/>
-    <div className="h-full w-[57%]">
-        <SignUpForm/>
-    </div>
-  </div>
-  
   );
 }
