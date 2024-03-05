46a5df8a44beb0b2310f886c77b0dff0b290fb57
Arhaan <arhaan1905@gmail.com>
2024-03-04 16:16:39 -0800

    added basic alignment

--- src/Login.jsx
+++ src/Login.jsx
@@ -4,27 +4,43 @@ import React, { useState } from "react";
 // create component
 // props is a way parent component can send values to their children
 export const Login = (props) => {
-    // add the state
-    const [email, setEmail] = useState('');
-    const [pw, setPw] = useState('');
+  // add the state
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
 
-    // when user submits form
-    const handleSubmit = (e) => {
-        // to make sure our page doesn't lose state when we reload the page
-        e.preventDefault();
-        console.log(email);
-    }
-    
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter your email" id="email" name="email"/>
-                <label htmlFor="password">password</label>
-                <input value={pw} onChange={(e) => setPw(e.target.value)} type="password" placeholder="Enter your password" id="password" name="password"/>
-                <button>Login</button>
-            </form>
-            <button onClick={props.onFormSwitch}>Don't have an account? Register here.</button>
-        </div>
-    )
-}
+  // when user submits form
+  const handleSubmit = (e) => {
+    // to make sure our page doesn't lose state when we reload the page
+    e.preventDefault();
+    console.log(email);
+  };
+
+  return (
+    <div className="auth-form-container">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Enter your email"
+          id="email"
+          name="email"
+        />
+        <label htmlFor="password">password</label>
+        <input
+          value={pw}
+          onChange={(e) => setPw(e.target.value)}
+          type="password"
+          placeholder="Enter your password"
+          id="password"
+          name="password"
+        />
+        <button>Login</button>
+      </form>
+      <button onClick={() => props.onFormSwitch("register")}>
+        Don't have an account? Register here.
+      </button>
+    </div>
+  );
+};
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,30 +1,53 @@
 // going to save user inputs, so use useState
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 // create component
-export const Register = () => {
-    const [email, setEmail] = useState('');
-    const [pw, setPw] = useState('');
-    const [name, setName] = useState('');
+export const Register = (props) => {
+  // add the state
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+  const [name, setName] = useState("");
 
-    const handleSubmit = (e) => {
-        // to make sure our page doesn't lose state when we reload the page
-        e.preventDefault();
-        console.log(email);
-    }
+  // when user submits form
+  const handleSubmit = (e) => {
+    // to make sure our page doesn't lose state when we reload the page
+    e.preventDefault();
+    console.log(email);
+  };
 
-    return(
-        <div>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="name">Full name</label>
-            <input value={name} name="name" id="name" placeholder="Enter you full name"/>
-            <label htmlFor="email">email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter your email" id="email" name="email"/>
-            <label htmlFor="password">password</label>
-            <input value={pw} onChange={(e) => setPw(e.target.value)} type="password" placeholder="Enter your password" id="password" name="password"/>
-            <button>Login</button>
-        </form>
-        <button>Already have an account? Login here.</button>
+  return (
+    <div className="auth-form-container">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Full name</label>
+        <input
+          value={name}
+          name="name"
+          id="name"
+          placeholder="Enter you full name"
+        />
+        <label htmlFor="email">email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Enter your email"
+          id="email"
+          name="email"
+        />
+        <label htmlFor="password">password</label>
+        <input
+          value={pw}
+          onChange={(e) => setPw(e.target.value)}
+          type="password"
+          placeholder="Enter your password"
+          id="password"
+          name="password"
+        />
+        <button>Login</button>
+      </form>
+      <button onClick={() => props.onFormSwitch("login")}>
+        Already have an account? Login here.
+      </button>
     </div>
-    )
-}
+  );
+};
