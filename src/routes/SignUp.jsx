3a91cf51570a54373c7e1ee774b6c8ef871d2f6e
Arhaan <arhaan1905@gmail.com>
2024-03-06 09:38:12 -0800

    addded role feature

--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -1,5 +1,10 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import SignUpForm from "../components/SignUpForm";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useNavigate } from "react-router-dom";
+
 export default function SignUp() {
+  const navigate = useNavigate();
   return (
     <div className="w-screen h-screen bg-white">
       <img
@@ -10,9 +15,15 @@ export default function SignUp() {
       <div className="h-full w-[57%]">
         <SignUpForm />
       </div>
-      <div className="w-full h-10 absolute bottom-0 flex flex-row justify-center">
+      <div className="w-full h-10 absolute bottom-0 flex flex-row justify-center text-xl">
         Already have an account?{" "}
-        <div className="text-dark-theme inline font-semibold ml-1 hover:underline hover:cursor-pointer">Sign In</div>
+        <div className="text-dark-theme inline font-semibold ml-1 hover:underline hover:cursor-pointer" onClick={() => navigate("/")}>Sign In</div>
+      </div>
+      <div>
+
+          <FontAwesomeIcon className="absolute bottom-10 right-20 w-10 h-10 bg-dark-theme flex flex-col justify-center items-center rounded-full hover:bg-light-theme border border-transparent hover:border-black text-white hover:text-black transition duration-500 hover:cursor-pointer p-5 hover:scale-125" icon={faArrowRight} />
+
+
       </div>
     </div>
   );
