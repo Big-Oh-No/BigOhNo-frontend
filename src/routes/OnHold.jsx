a55b64ef54c9f43c3b2e8c7db8c64bb5e280a0cc
moon <113469325+Moon0256@users.noreply.github.com>
2024-03-09 13:42:11 -0800

    pushed

--- src/routes/OnHold.jsx
+++ src/routes/OnHold.jsx
@@ -1,10 +1,11 @@
 export default function OnHold() {
   return <div className="h-screen w-screen flex justify-center items-center">
-    <div  className="text-7xl font-bold absolute top-36 left-32 font-inter"/>
-    <Verification Pending/>
+   <div className="">
+            <DasboardLayout/>
+        </div>
     <img
     src={require("../assets/verifff.png")}
     alt="Verification Pending"
-    className="h-full"/>
+    className=""/>
     </div>;
 }
