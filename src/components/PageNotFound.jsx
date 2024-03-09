6cac374b9c143c6534f9d87e3d1deb939d4d6ef0
Arhaan <arhaan1905@gmail.com>
2024-03-08 21:30:11 -0800

    added more enhancements

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function PageNotFound() {
   return (
-    <div className="font-inter font-bold text-4xl pt-20 pl-10">
-        Sorry, <div className="text-dark-theme inline">404</div> page not found.
+    <div className="font-inter font-bold text-4xl pt-20 pl-10 flex flex-col">
+        <div>Whoops, you seem lost!</div>
+        <div className=" mt-32 text-dark-theme text-9xl pl-16 inline">404 Not Found</div>
+        
     </div>
   );
 }
