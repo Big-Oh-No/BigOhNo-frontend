56a9d83a43d55580c70468b63b959cf29de2c683
Arhaan <arhaan1905@gmail.com>
2024-03-08 21:16:02 -0800

    enhanced 404 message

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function PageNotFound() {
   return (
     <div className="font-inter font-bold text-4xl pt-20 pl-10">
-        Sorry, page not found.
+        Sorry, <div className="text-dark-theme inline">404</div> page not found.
     </div>
   );
 }
