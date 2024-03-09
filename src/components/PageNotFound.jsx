1e93d6a57e99dca3f05abd524418f1ef7309920b
Arhaan <113068699+arhaankk@users.noreply.github.com>
2024-03-08 21:47:46 -0800

    26 create a 404 not found page (#27)
    * added the error screen with placement issue
    
    * fixed animation placement
    
    * enhanced 404 message
    
    * added more enhancements

---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function PageNotFound() {
+  return (
+    <div className="font-inter font-bold text-4xl pt-20 pl-10 flex flex-col">
+        <div>Whoops, you seem lost!</div>
+        <div className=" mt-32 text-dark-theme text-9xl pl-16 inline">404 Not Found</div>
+        
+    </div>
+  );
+}
--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,12 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import DasboardLayout from "../components/DashboardLayout";
 
-export default function Dashboard(){
-    const navigate = useNavigate();
+export default function Dashboard() {
+  const navigate = useNavigate();
 
-    return(
-        <div className="">
-            <DasboardLayout/>
-        </div>
-    );
+  return (
+    <div className="">
+      <DasboardLayout />
+    </div>
+  );
 }
