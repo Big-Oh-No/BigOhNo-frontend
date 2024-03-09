5697896697e9e281eef22ce05880b9c9c3058e32
Arhaan <arhaan1905@gmail.com>
2024-03-08 21:11:03 -0800

    fixed animation placement

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -3,11 +3,11 @@ import PageNotFound from "../components/PageNotFound";
 export default function NotFound() {
   return (
     <div className="flex flex-row-reverse w-screen h-screen">
-        <div className="relative h-full w-[50%]">
+        <div className="relative h-full w-[70%]">
         <img
         src={require("../assets/page_notfound.png")}
         alt="404 Page"
-        className="absolute bottom-7"
+        className="absolute top-20 right-0 h-full w-full -mr-60 "
       />
         </div>
         <div className="h-full w-[50%]">
