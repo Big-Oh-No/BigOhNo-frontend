30420c8d48b9905d64b0f5dc86252a7f644a9394
Arhaan <arhaan1905@gmail.com>
2024-03-17 15:52:17 -0700

    added basic table template

--- src/components/Admin/AdminVerify.jsx
+++ src/components/Admin/AdminVerify.jsx
@@ -1,13 +1,22 @@
 export default function AdminVerify() {
-  return <div>
-    <table>
-        <thead>
+    return (
+      <div>
+        <h2 className="text-dark-theme font-inter font-semibold text-4xl mt-8 ml-5">Pending Verification</h2>
+        <table className="border-collapse border border-gray-400 mt-4 ml-5">
+          <thead>
             <tr>
-                <thead>Student Name</thead>
-                <thead>Verification Status</thead>
+              <th className="border border-gray-400 px-4 py-2">Student Name</th>
+              <th className="border border-gray-400 px-4 py-2">Verification Status</th>
             </tr>
-        </thead>
-        
-    </table>
-  </div>;
-}
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border border-gray-400 px-4 py-2">John Appleseed</td>
+              <td className="border border-gray-400 px-4 py-2">Pending</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  
