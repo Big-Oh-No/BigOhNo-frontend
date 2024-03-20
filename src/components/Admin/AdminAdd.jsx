45ad2d43a9ca09ea54b90cc9fe60e2f171cfc2a2
Arhaan <arhaan1905@gmail.com>
2024-03-20 03:09:50 -0700

    added verification - pending

--- src/components/Admin/AdminAdd.jsx
+++ src/components/Admin/AdminAdd.jsx
@@ -1,3 +1,217 @@
+import React, { useState } from "react";
+
 export default function AdminAdd() {
-    return <div className="text-xl">THIS IS ADMIN ADD</div>
+  const [formData, setFormData] = useState({
+    dpt: "",
+    code: "",
+    name: "",
+    desc: "",
+    syllabus: null,
+    courseImg: null,
+    term: "",
+    year: 2024,
+    credits: 0,
+    totalSeats: 0,
+    teacherEmail: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+  };
+
+  return (
+    <div className="flex flex-col w-full h-full px-20 pt-20 ">
+      <div className="w-[45%]">
+        <h2 className="text-5xl font-semibold mb-4 font-inter ">Course Form</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mt-8 space-y-2">
+            <label className="block font-semibold text-xl">Department</label>
+            <input
+              className="px-5 w-full focus:outline-none bg-light-theme border-[0.075rem] border-black h-14 text-lg rounded-xl"
+              type="text"
+              id="dpt"
+              name="dpt"
+              value={formData.dpt}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2">
+              Course Code
+            </label>
+            <input
+              className="px-5 w-full focus:outline-none bg-light-theme border-[0.075rem] border-black h-14 text-lg rounded-xl"
+              type="text"
+              id="code"
+              name="code"
+              value={formData.code}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2">
+              Course Name
+            </label>
+            <input
+              className="px-5 w-full focus:outline-none bg-light-theme border-[0.075rem] border-black h-14 text-lg rounded-xl"
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2">
+              Description
+            </label>
+            <textarea
+              className="px-5 w-full focus:outline-none bg-light-theme border-[0.075rem] border-black h-14 text-lg rounded-xl"
+              id="desc"
+              name="desc"
+              value={formData.desc}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2">
+              Syllabus
+            </label>
+            <input
+              className="border border-gray-400 rounded px-3 py-2 w-full"
+              type="file"
+              id="syllabus"
+              name="syllabus"
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="courseImg"
+            >
+              Course Image
+            </label>
+            <input
+              className="border border-gray-400 rounded px-3 py-2 w-full"
+              type="file"
+              id="courseImg"
+              name="courseImg"
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2">Term</label>
+            <select
+              className="border border-gray-400 rounded px-3 py-2 w-full"
+              id="term"
+              name="term"
+              value={formData.term}
+              onChange={handleChange}
+              required
+            >
+              <option value="">Select Term</option>
+              <option value="W1">W1</option>
+              <option value="W2">W2</option>
+              <option value="S1">S1</option>
+              <option value="S2">S2</option>
+            </select>
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2">Year</label>
+            <input
+              className="border border-gray-400 rounded px-3 py-2 w-full"
+              type="number"
+              id="year"
+              name="year"
+              value={formData.year}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="credits"
+            >
+              Credits
+            </label>
+            <input
+              className="border border-gray-400 rounded px-3 py-2 w-full"
+              type="number"
+              id="credits"
+              name="credits"
+              value={formData.credits}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2">
+              Total Seats
+            </label>
+            <input
+              className="border border-gray-400 rounded px-3 py-2 w-full"
+              type="number"
+              id="totalSeats"
+              name="totalSeats"
+              value={formData.totalSeats}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="teacherEmail"
+            >
+              Teacher Email
+            </label>
+            <input
+              className="border border-gray-400 rounded px-3 py-2 w-full"
+              type="email"
+              id="teacherEmail"
+              name="teacherEmail"
+              value={formData.teacherEmail}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="flex justify-center">
+            <div className="flex items-center justify-center w-[25%] mt-16 rounded-full border-black font-inter text-2xl bg-dark-theme font-semibold hover:cursor-pointer py-3 hover:bg-light-theme border border-transparent hover:border-black text-white hover:text-black transition duration-500 hover:scale-125 select-none">
+              <button type="submit" className="">
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className="h-[13%]"></div>
+      </div>
+    </div>
+  );
 }
--- src/components/Admin/AdminVerify.jsx
+++ src/components/Admin/AdminVerify.jsx
@@ -1,33 +1,136 @@
 import { useState } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import VerificationRequests from "../../models/verification_requests";
 
 export default function AdminVerify() {
-  const [status, setStatus] = useState("Pending")
-  const handleSubmit = () => {
-    setStatus("Approved")
-  }
-    return (
-    <div>
-      <h2 className="text-dark-theme font-inter font-semibold text-4xl mt-8 ml-5">
+  const [requests, setRequests] = useState([]);
+  const navigate = useNavigate();
+  const handleApprove = async (user_email, name) => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_BACKEND}/user/verify`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: localStorage.getItem("AuthCookie")["email"],
+            password: localStorage.getItem("AuthCookie")["password"],
+            user_email: user_email,
+          }),
+        }
+      );
+
+      if (response.status === 200) {
+        init();
+        showAlert(`${name} verified sucessfully!`);
+      } else {
+        const detail = await response.json();
+        showAlert(detail["detail"]);
+      }
+    } catch (error) {
+      showAlert("Unexpected error occurred");
+    }
+  };
+
+  const showAlert = (message) => {
+    alert(`${message}`);
+  };
+  useEffect(() => init, []);
+  const init = async () => {
+    const data = JSON.parse(localStorage.getItem("AuthCookie"));
+
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_BACKEND}/user/verification_status`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: data["email"],
+            password: data["password"],
+          }),
+        }
+      );
+
+      if (response.status === 200) {
+        const response_json = await response.json();
+        console.log(response_json.length);
+        let request_array = [];
+        for (let i = 0; i < response_json.length; ++i) {
+          const current_request = new VerificationRequests({
+            firstName: response_json[i].first_name,
+            lastName: response_json[i].last_name,
+            email: response_json[i].email,
+            role: response_json[i].role,
+          });
+          request_array.push(current_request);
+        }
+        setRequests(request_array);
+      } else {
+        localStorage.removeItem("AuthCookie");
+        navigate("/");
+        return;
+      }
+    } catch (error) {
+      return;
+    }
+  };
+
+  return (
+    <div className="w-screen h-screen flex flex-col">
+      <div className="text-5xl font-bold font-inter pt-10 w-full pl-10 pb-10 z-20">
         Pending Verification
-      </h2>
-      <table className="border-collapse border border-gray-400 mt-10 ml-10">
-        <thead>
-          <tr>
-            <th className="border border-gray-400 px-4 py-2">Student Name</th>
-            <th className="border border-gray-400 px-4 py-2">
-              Verification Status
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="border border-gray-400 px-4 py-2">John Appleseed</td>
-            <td className="border border-gray-400 px-4 py-2">
-              <div className={`flex flex-row justify-center items-center rounded-md hover:cursor-pointer h-9 text-white ${status === "Pending" ? "bg-black"  : "bg-green-600"}`} onClick={handleSubmit}>{status}</div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      </div>
+      <div className="pt-5 flex flex-col justify-center w-full p-10">
+        <table className="border border-gray-400">
+          <thead className="text-2xl text-white bg-dark-theme">
+            <tr>
+              <th className="border border-gray-400 px-4 py-2">First Name</th>
+              <th className="border border-gray-400 px-4 py-2">Last Name</th>
+              <th className="border border-gray-400 px-4 py-2">Email</th>
+              <th className="border border-gray-400 px-4 py-2">Role</th>
+              <th className="border border-gray-400 px-4 py-2">Status</th>
+            </tr>
+          </thead>
+          <tbody className="text-xl">
+            {requests.map((request) => {
+              return (
+                <tr className="hover:bg-light-theme">
+                  <td className="border border-gray-400 px-4 py-2">
+                    {request.firstName}
+                  </td>
+                  <td className="border border-gray-400 px-4 py-2">
+                    {request.lastName}
+                  </td>
+                  <td className="border border-gray-400 px-4 py-2">
+                    {request.email}
+                  </td>
+                  <td className="border border-gray-400 px-4 py-2">
+                    {request.role}
+                  </td>
+                  <td className="border border-gray-400">
+                    <div className=" w-full h-full justify-around py-3">
+                      <div className="text-3xl">
+                        <div
+                          className="flex justify-center hover:cursor-pointer"
+                          onClick={() => {handleApprove(request.email, `${request.firstName} ${request.lastName}`)}}
+                        >
+                          ✅
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
