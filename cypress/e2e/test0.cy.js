f2e82adf045431b3cf0e46281671e168cf0bcb0d
Appurls <devarka403@gmail.com>
2024-03-19 20:34:26 -0700

    done setting up cypress

---
+++ cypress/e2e/test0.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress"/>
+
+describe('Visit Localhost Website', () => {
+
+before(() => {
+    // Start the development server before running tests
+    cy.exec('npm run start'); // Replace this command with the one you use to start your development server
+  });
+
+
+it('Open Website', () => {
+
+    
+    cy.visit('http://localhost:3000');
+})
+
+});
