3923a1d55f2dcb0d5c22438e8907ab6d05495fe4
Appurls <devarka403@gmail.com>
2024-03-19 20:42:21 -0700

    completed setup

--- cypress/e2e/test0.cy.js
+++ cypress/e2e/test0.cy.js
@@ -1,17 +1,17 @@
 /// <reference types="cypress"/>
 
-describe('Visit Localhost Website', () => {
+// describe('Visit Localhost Website', () => {
 
-before(() => {
-    // Start the development server before running tests
-    cy.exec('npm run start'); // Replace this command with the one you use to start your development server
-  });
+//     before(() => {
+//     // Start the development server before running tests
+//     cy.exec('npm run start'); // Replace this command with the one you use to start your development server
+//   });
 
 
-it('Open Website', () => {
+// it('Open Website', () => {
 
     
-    cy.visit('http://localhost:3000');
-})
+//     cy.visit('http://localhost:3000');
+// })
 
-});
+// });
