e5f76b4905c7ce23f9a9ee4dd064cac913d5d7c1
Arhaan <arhaan1905@gmail.com>
2024-03-21 14:55:56 -0700

    restored to defaults

--- src/components/Student/StudentCourseCard.jsx
+++ src/components/Student/StudentCourseCard.jsx
@@ -1,32 +1,24 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import InvalidPage from "../common/InvalidPage";
+
 
 export default function StudentCourseCard(props) {
   const navigate = useNavigate();
   const [isHovered, setIsHovered] = useState(false);
 
-  const handleCourseClick = () => {
-    const idArr = [9];
-    if (idArr.includes(props.course.id)){
-      navigate(`/course/${props.course.id}`);
-    }else{
-      navigate("InvalidPage");
-    }
-  }
   return (
     <div
       className="bg-white border-[2.9px] border-neutral-200 w-[85%] h-[85%] rounded-2xl hover:scale-110 hover:shadow-2xl p-2 transition duration-500 hover:cursor-pointer"
       style={{ boxShadow: isHovered ? "0 0 50px rgba(0, 0, 0, 0.25)" : "none" }}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      onClick={() => handleCourseClick}
+      onClick={() => navigate(`/course/${props.course.id}`)}
     >
       <div className="h-[60%] overflow-clip flex items-center justify-center rounded-xl">
       {props.course.image_url ? (
         <img className="rounded-xl" src={props.course.image_url} alt="course_image"/>
       ) : (
-        <img className="rounded-xl" src={require("../../assets/course.jpeg")} alt="course_image"/>
+        <img className="rounded-xl" src={require("../../assets/course.jpeg")} alt="corse_image"/>
       )}
       </div>
       <div className="text-2xl p-2 mt-5 font-semibold font-inter truncate w-full">
