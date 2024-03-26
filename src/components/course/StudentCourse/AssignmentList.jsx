d0aa8dad81eb4fd187f091814dbcab050dac6863
moon <113469325+Moon0256@users.noreply.github.com>
2024-03-25 21:21:49 -0700

    added cards

--- src/components/course/StudentCourse/AssignmentList.jsx
+++ src/components/course/StudentCourse/AssignmentList.jsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AssignmentCard from "./AssignmentCard";
+
 export default function AssignmentList() {
   const navigate = useNavigate();
+  const [showFileUpload, setShowFileUpload] = useState({});
+  const [buttonVisibility, setButtonVisibility] = useState({});
+
   const handleClick = (assignmentId) => {
     // navigate to the assignment card
     navigate("/assignments");
-  };
-  const [showFileUpload, setShowFileUpload] = useState(false);
 
-  const handleFileUpload = () => {
-    setShowFileUpload(true);
+    // Toggle button visibility for the clicked assignment
+    setButtonVisibility((prevState) => ({
+      prevState,
+      [assignmentId]: !prevState[assignmentId],
+    }));
+  };
+
+  const handleFileUpload = (assignmentId) => {
+    setShowFileUpload((prevState) => ({
+      prevState,
+      [assignmentId]: true,
+    }));
   };
 
   return (
@@ -21,7 +33,7 @@ export default function AssignmentList() {
       <div className="flex justify-center">
         <div className="w-full max-w-screen-2xl pt-44">
           <ul className="list-none p-0">
-          <li
+            <li
               className="py-6 px-8 border-b shadow-xl cursor-pointer flex flex-col justify-between"
               onClick={() => handleClick(1)}
             >
@@ -30,22 +42,24 @@ export default function AssignmentList() {
                   <div className="text-3xl font-semibold mb-2">
                     Assignment 1
                   </div>
-                  <div className="text-xl">Due date: </div>
+                  <div className="text-lg">Due date: </div>
                 </div>
-                <div className="text-2xl text-right">Grade: </div>
+                <div className="text-xl text-right">Grade: </div>
               </div>
-              <div className="pt-6 flex justify-between">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white text-xl font-inter  py-3 px-4 rounded-xl">
-                  Download Assignment
-                </button>
-                <button
-                  onClick={handleFileUpload}
-                  className="bg-green-700 hover:bg-green-900 text-white text-xl font-inter  py-3 px-4 rounded-xl"
-                >
-                  Submit Assignment
-                </button>
-              </div>
-              {showFileUpload && (
+              {buttonVisibility[1] && (
+                <div className="pt-6 flex justify-between">
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white text-xl font-inter  py-3 px-4 rounded-xl">
+                    Download Assignment
+                  </button>
+                  <button
+                    onClick={() => handleFileUpload(1)}
+                    className="bg-green-700 hover:bg-green-900 text-white text-xl font-inter  py-3 px-4 rounded-xl"
+                  >
+                    Submit Assignment
+                  </button>
+                </div>
+              )}
+              {showFileUpload[1] && (
                 <div className="pt-6 flex justify-center">
                   <input
                     type="file"
@@ -58,29 +72,31 @@ export default function AssignmentList() {
 
             <li
               className="py-6 px-8 border-b shadow-xl cursor-pointer flex flex-col justify-between"
-              onClick={() => handleClick(1)}
+              onClick={() => handleClick(2)}
             >
               <div className="flex justify-between">
                 <div>
                   <div className="text-3xl font-semibold mb-2">
                     Assignment 2
                   </div>
-                  <div className="text-xl">Due date: </div>
+                  <div className="text-lg">Due date: </div>
                 </div>
-                <div className="text-2xl text-right">Grade: </div>
+                <div className="text-xl text-right">Grade: </div>
               </div>
-              <div className="pt-6 flex justify-between">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white text-xl font-inter  py-3 px-4 rounded-xl">
-                  Download Assignment
-                </button>
-                <button
-                  onClick={handleFileUpload}
-                  className="bg-green-700 hover:bg-green-900 text-white text-xl font-inter  py-3 px-4 rounded-xl"
-                >
-                  Submit Assignment
-                </button>
-              </div>
-              {showFileUpload && (
+              {buttonVisibility[2] && (
+                <div className="pt-6 flex justify-between">
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white text-xl font-inter  py-3 px-4 rounded-xl">
+                    Download Assignment
+                  </button>
+                  <button
+                    onClick={() => handleFileUpload(2)}
+                    className="bg-green-700 hover:bg-green-900 text-white text-xl font-inter  py-3 px-4 rounded-xl"
+                  >
+                    Submit Assignment
+                  </button>
+                </div>
+              )}
+              {showFileUpload[2] && (
                 <div className="pt-6 flex justify-center">
                   <input
                     type="file"
@@ -102,28 +118,16 @@ export default function AssignmentList() {
 // import { useState } from "react";
 // import { useNavigate } from "react-router-dom";
 // import AssignmentCard from "./AssignmentCard";
-
 // export default function AssignmentList() {
 //   const navigate = useNavigate();
-//   const [showFileUpload, setShowFileUpload] = useState({});
-//   const [buttonVisibility, setButtonVisibility] = useState({});
-
 //   const handleClick = (assignmentId) => {
 //     // navigate to the assignment card
 //     navigate("/assignments");
-
-//     // Toggle button visibility for the clicked assignment
-//     setButtonVisibility((prevState) => ({
-//       prevState,
-//       [assignmentId]: !prevState[assignmentId],
-//     }));
 //   };
+//   const [showFileUpload, setShowFileUpload] = useState(false);
 
-//   const handleFileUpload = (assignmentId) => {
-//     setShowFileUpload((prevState) => ({
-//       prevState,
-//       [assignmentId]: true,
-//     }));
+//   const handleFileUpload = () => {
+//     setShowFileUpload(true);
 //   };
 
 //   return (
@@ -134,7 +138,7 @@ export default function AssignmentList() {
 //       <div className="flex justify-center">
 //         <div className="w-full max-w-screen-2xl pt-44">
 //           <ul className="list-none p-0">
-//             <li
+//           <li
 //               className="py-6 px-8 border-b shadow-xl cursor-pointer flex flex-col justify-between"
 //               onClick={() => handleClick(1)}
 //             >
@@ -143,24 +147,22 @@ export default function AssignmentList() {
 //                   <div className="text-3xl font-semibold mb-2">
 //                     Assignment 1
 //                   </div>
-//                   <div className="text-lg">Due date: </div>
+//                   <div className="text-xl">Due date: </div>
 //                 </div>
-//                 <div className="text-xl text-right">Grade: </div>
+//                 <div className="text-2xl text-right">Grade: </div>
 //               </div>
-//               {buttonVisibility[1] && (
-//                 <div className="pt-6 flex justify-between">
-//                   <button className="bg-blue-500 hover:bg-blue-700 text-white text-xl font-inter  py-3 px-4 rounded-xl">
-//                     Download Assignment
-//                   </button>
-//                   <button
-//                     onClick={() => handleFileUpload(1)}
-//                     className="bg-green-700 hover:bg-green-900 text-white text-xl font-inter  py-3 px-4 rounded-xl"
-//                   >
-//                     Submit Assignment
-//                   </button>
-//                 </div>
-//               )}
-//               {showFileUpload[1] && (
+//               <div className="pt-6 flex justify-between">
+//                 <button className="bg-blue-500 hover:bg-blue-700 text-white text-xl font-inter  py-3 px-4 rounded-xl">
+//                   Download Assignment
+//                 </button>
+//                 <button
+//                   onClick={handleFileUpload}
+//                   className="bg-green-700 hover:bg-green-900 text-white text-xl font-inter  py-3 px-4 rounded-xl"
+//                 >
+//                   Submit Assignment
+//                 </button>
+//               </div>
+//               {showFileUpload && (
 //                 <div className="pt-6 flex justify-center">
 //                   <input
 //                     type="file"
@@ -173,31 +175,29 @@ export default function AssignmentList() {
 
 //             <li
 //               className="py-6 px-8 border-b shadow-xl cursor-pointer flex flex-col justify-between"
-//               onClick={() => handleClick(2)}
+//               onClick={() => handleClick(1)}
 //             >
 //               <div className="flex justify-between">
 //                 <div>
 //                   <div className="text-3xl font-semibold mb-2">
 //                     Assignment 2
 //                   </div>
-//                   <div className="text-lg">Due date: </div>
+//                   <div className="text-xl">Due date: </div>
 //                 </div>
-//                 <div className="text-xl text-right">Grade: </div>
+//                 <div className="text-2xl text-right">Grade: </div>
 //               </div>
-//               {buttonVisibility[2] && (
-//                 <div className="pt-6 flex justify-between">
-//                   <button className="bg-blue-500 hover:bg-blue-700 text-white text-xl font-inter  py-3 px-4 rounded-xl">
-//                     Download Assignment
-//                   </button>
-//                   <button
-//                     onClick={() => handleFileUpload(2)}
-//                     className="bg-green-700 hover:bg-green-900 text-white text-xl font-inter  py-3 px-4 rounded-xl"
-//                   >
-//                     Submit Assignment
-//                   </button>
-//                 </div>
-//               )}
-//               {showFileUpload[2] && (
+//               <div className="pt-6 flex justify-between">
+//                 <button className="bg-blue-500 hover:bg-blue-700 text-white text-xl font-inter  py-3 px-4 rounded-xl">
+//                   Download Assignment
+//                 </button>
+//                 <button
+//                   onClick={handleFileUpload}
+//                   className="bg-green-700 hover:bg-green-900 text-white text-xl font-inter  py-3 px-4 rounded-xl"
+//                 >
+//                   Submit Assignment
+//                 </button>
+//               </div>
+//               {showFileUpload && (
 //                 <div className="pt-6 flex justify-center">
 //                   <input
 //                     type="file"
@@ -213,7 +213,4 @@ export default function AssignmentList() {
 //     </>
 //   );
 // }
-
-
-
 
---
+++ src/components/course/Syllabus.jsx
@@ -0,0 +1,13 @@
+export default function Syllabus(){
+    return(
+        <>
+        <div className="flex flex-row">
+            <div>Course Name</div>
+            <div>Instructor Name</div>
+            <div>Syllabus</div>
+
+        </div>
+        
+        </>
+    )
+}
