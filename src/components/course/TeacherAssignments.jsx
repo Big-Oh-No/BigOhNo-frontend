33d56e49e789e036442a98a3afdfbbe86f6bacc3
Arhaan <113068699+arhaankk@users.noreply.github.com>
2024-03-27 06:42:26 -0700

    Teacher assignment (#101)
    * added assignment card
    
    * added connections with score page
    
    * added grades component
    
    ---------
    
    Co-authored-by: Anand <clef@Anands-MacBook-Pro.local>

--- src/components/course/TeacherAssignments.jsx
+++ src/components/course/TeacherAssignments.jsx
@@ -1,26 +1,45 @@
+import React, { useState } from "react";
+import TeacherAssignmentCard from "./TeacherCourse/TeacherAssignmentCard";
+import TeacherAssignmentTable from "./TeacherCourse/TeacherAssignmentTable";
 
-import React from 'react'
-
-export default function TeacherAssignments() {
+export default function TeacherAssignments(props) {
+  const [showSubmission, setShowSubmission] = useState(false);
+  const [assignment, setAssignment] = useState();
+  const cardClickHandler = (assignment) => {
+    setAssignment(assignment);
+    setShowSubmission(true);
+  };
+  const cardCloseHandler = () => {
+    setAssignment(null);
+    setShowSubmission(false);
+  };
   return (
-    <>
-     <div className="text-5xl font-bold font-inter pt-10 w-full pl-10 pb-10 bg-dark-theme z-20 text-white fixed">
-        Assignments
-      </div>
-      <div className='pt-10 w-screen'>
-        {/* If no assignments, then show this lol */}
-        {/* <div className='w-full h-full  flex justify-center items-center text-2xl'>No Assignments available</div> */}
-
-        {/* else... */}
-        <div className='flex w-full justify-center mt-[7rem]'>
-            <div className='flex flex-col w-full px-32'>
-                <div className='w-full h-[19rem] flex justify-center items-center'>
-                    
-                </div>
+    <div>
+      {showSubmission && assignment !== null ? (
+        <TeacherAssignmentTable assignment={assignment} onClick={cardCloseHandler}/>
+      ) : (
+        <div>
+          <div className="text-5xl font-bold font-inter pt-10 w-full top-0 pl-10 pb-10 bg-dark-theme z-20 text-white fixed">
+            Assignments
+          </div>
+          <div className="h-screen w-screen pt-11">
+            <div className="flex h-full w-full justify-center mt-[7rem]">
+              <div className="flex flex-col items-center h-full w-full px-32 space-y-5">
+                {props.data.assignments.map((e) => {
+                  return (
+                    <div className="w-[70%] h-[20%] flex justify-center items-center">
+                      <TeacherAssignmentCard
+                        assignment={e}
+                        onClick={cardClickHandler}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
             </div>
+          </div>
         </div>
-      </div>
-    </>
-   
-  )
+      )}
+    </div>
+  );
 }
