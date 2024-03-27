2d4236c53e46925f64c909f6f802da3479667f7f
Arhaan <arhaan1905@gmail.com>
2024-03-27 07:15:02 -0700

    added assignmnet page

--- src/components/course/TeacherCourse/TeacherAssignmentCard.jsx
+++ src/components/course/TeacherCourse/TeacherAssignmentCard.jsx
@@ -21,7 +21,7 @@ function TeacherAssignmentCard(props) {
         </div>
         <div className="pt-1">Published on: {props.assignment.published}</div>
         <div className="flex flex-row justify-between items-end h-full">
-        <a href={props.assignment.file_url}
+        <a href={props.assignment.file_url} target="_blank"
         className="flex justify-center items-center text-white bg-dark-theme px-5 py-2 z-10 rounded-xl text-xl hover:text-black hover:bg-light-theme border hover:cursor-pointer hover:scale-105 hover:shadow-lg hover:border-black border-transparent transition duration-200" >Download</a>
           <div className="font-inter text-xl font-semibold">
             {props.assignment.total_grade} marks
